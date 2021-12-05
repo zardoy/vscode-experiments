d9b439ac9e6688caac8a684d1dac36a07803fc90
Vitaly Turovsky <vital2580@icloud.com>
2021-12-05 14:07:58 +0300

    feat: `goToNextErrorInFile` command

--- src/extension.ts
+++ src/extension.ts
@@ -30,6 +30,24 @@ export const activate = () => {
         await vscode.env.openExternal(vscode.Uri.parse(url))
     })
 
+    registerExtensionCommand('goToNextErrorInFile', async (_, url: string) => {
+        const activeEditor = vscode.window.activeTextEditor
+        if (!activeEditor || activeEditor.viewColumn === undefined) return
+        const diagnostics = vscode.languages.getDiagnostics(activeEditor.document.uri)
+        const errors = diagnostics.filter(({ severity }) => severity === vscode.DiagnosticSeverity.Error)
+        let nextRange = errors[0]?.range
+        for (const { range } of errors) {
+            if (!range.start.isAfter(activeEditor.selection.end)) continue
+            nextRange = range
+            break
+        }
+
+        if (!nextRange) return
+        activeEditor.selections = [new vscode.Selection(nextRange.start, nextRange.end)]
+        activeEditor.revealRange(activeEditor.selection)
+        await vscode.commands.executeCommand('editor.action.marker.next')
+    })
+
     if (process.env.NODE_ENV !== 'development') return
     registerActiveDevelopmentCommand(() => {
         const decoration = vscode.window.createTextEditorDecorationType({
