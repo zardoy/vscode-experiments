18daf42eb644305fc45120d4d9331ae83a35abbe
Vitaly Turovsky <vital2580@icloud.com>
2021-12-12 18:32:58 +0300

    fix: Revamp signature completions. They now correctly parse args with callbacks

--- src/features/signatureCompletions.ts
+++ src/features/signatureCompletions.ts
@@ -10,27 +10,26 @@ export const registerSignatureCompletions = () => {
             const surroundingText = document.getText(new vscode.Range(position.translate(0, -1), position.translate(0, 1)))
             // TODO also provide other detections
             if (!(surroundingText.startsWith('(') || surroundingText.endsWith(')'))) return
-            console.time('exec')
             const result: vscode.SignatureHelp = await vscode.commands.executeCommand('vscode.executeSignatureHelpProvider', document.uri, position)
-            console.timeEnd('exec')
             if (!result) return []
-            // TODO! support parsing by label
             // suggestions should be matched against selected signature by user
             const signature = result.signatures[result.activeSignature]!
-            const argsString = /\((.+)\)/.exec(signature.label)?.[1]
-            if (!argsString) return []
+            // const argsString = /\((.+)\)/.exec(signature.label)?.[1]
             // TS-aware only
-            const args = argsString.split(',').map(str =>
-                str
-                    .replace(/(.+):.+/, '$1')
-                    .replace(/\?$/, '')
-                    .trim(),
-            )
+            const args = signature.parameters
+                .map(({ label }) => (typeof label === 'string' ? label : signature.label.slice(...label)))
+                .map(str => {
+                    console.log(str)
+                    return str
+                        .replace(/(.+?):.+/, '$1')
+                        .replace(/\?$/, '')
+                        .trim()
+                })
             const completions = [] as vscode.CompletionItem[]
             let prevSuggestionText = ''
             for (const arg of args.slice(result.activeParameter)) {
                 const label = `${prevSuggestionText}${arg}`
-                const completion = new vscode.CompletionItem({ label, description: 'SIGNATURE COMPLETION' }, vscode.CompletionItemKind.Field)
+                const completion = new vscode.CompletionItem({ label, description: 'SIGNATURE' }, vscode.CompletionItemKind.Field)
                 completion.sortText = '!100'
                 prevSuggestionText = `${label}, `
                 completion.insertText = prevSuggestionText
