49352cc1cc576ed579bc5129de3df771b3411ff9
Vitaly Turovsky <vital2580@icloud.com>
2022-01-03 02:25:18 +0300

    fix: also publish to the web

--- vscode-framework.config.js
+++ vscode-framework.config.js
@@ -2,9 +2,16 @@
 
 /** @type{import('vscode-framework/build/config').UserConfig} */
 const config = {
-    development: {
-        disableExtensions: true,
+    esbuild: {
+        production: {
+            defineEnv: {
+                EXTENSION_BOOTSTRAP_CONFIG: null,
+            },
+        },
     },
+    // target: 'web',
+    // webOpen: 'web',
+    target: { desktop: true, web: true },
 }
 
 module.exports = config
