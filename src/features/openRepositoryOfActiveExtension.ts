8c1288849d2e86cdd41930b48e47b38ee62f7ac5
Vitaly Turovsky <vital2580@icloud.com>
2022-03-04 11:24:14 +0300

    ci: another fix for web

--- src/features/openRepositoryOfActiveExtension.ts
+++ src/features/openRepositoryOfActiveExtension.ts
@@ -3,34 +3,34 @@ import { registerExtensionCommand, showQuickPick } from 'vscode-framework'
 
 export const registerOpenRepositoryOfActiveExtension = async () => {
     registerExtensionCommand('openRepositoryOfActiveExtension', async () => {
-        if (process.env.PLATFORM !== 'node') {
+        // eslint-disable-next-line no-negated-condition
+        if (process.env.PLATFORM !== 'web') {
+            const { fromUrl } = await import('hosted-git-info')
+            // TODO copy id, link button
+            const extensionId = await showQuickPick(
+                vscode.extensions.all.map(({ id, packageJSON }) => {
+                    let label = packageJSON.displayName
+                    if (!packageJSON.repository) label = `[no repository] ${label}`
+                    return { value: id, label, description: id }
+                }),
+                { matchOnDescription: true, title: 'Open Repository of Active Extension' },
+            )
+            if (extensionId === undefined) return
+            let { repository } = vscode.extensions.getExtension(extensionId)!.packageJSON
+            if (!repository) return
+            let repoDir: string | undefined
+            if (typeof repository === 'object') {
+                repoDir = repository.directory
+                repository = repository.url
+            }
+
+            const repo = fromUrl(repository)!
+            let urlPath = ''
+            if (repo.domain === 'github.com' && repoDir) urlPath = `/tree/master/${repoDir}`
+
+            await vscode.env.openExternal(`${repo.browse()}${urlPath}` as any)
+        } else {
             void vscode.window.showWarningMessage('Web is not supported by this feature')
-            return
         }
-
-        const { fromUrl } = await import('hosted-git-info')
-        // TODO copy id, link button
-        const extensionId = await showQuickPick(
-            vscode.extensions.all.map(({ id, packageJSON }) => {
-                let label = packageJSON.displayName
-                if (!packageJSON.repository) label = `[no repository] ${label}`
-                return { value: id, label, description: id }
-            }),
-            { matchOnDescription: true, title: 'Open Repository of Active Extension' },
-        )
-        if (extensionId === undefined) return
-        let { repository } = vscode.extensions.getExtension(extensionId)!.packageJSON
-        if (!repository) return
-        let repoDir: string | undefined
-        if (typeof repository === 'object') {
-            repoDir = repository.directory
-            repository = repository.url
-        }
-
-        const repo = fromUrl(repository)!
-        let urlPath = ''
-        if (repo.domain === 'github.com' && repoDir) urlPath = `/tree/master/${repoDir}`
-
-        await vscode.env.openExternal(`${repo.browse()}${urlPath}` as any)
     })
 }
