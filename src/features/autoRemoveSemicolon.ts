ea109fd486c75e83aa05c32a95679fb5694a1793
Ilya Golovin <ilya.golovin.14.01.12@gmail.com>
2022-06-07 16:36:56 +0300

    trigger suggestions

--- src/features/autoRemoveSemicolon.ts
+++ src/features/autoRemoveSemicolon.ts
@@ -3,7 +3,7 @@ import { equals } from 'rambda'
 import { defaultJsSupersetLangsWithVue } from '@zardoy/vscode-utils/build/langs'
 
 export default () => {
-    vscode.workspace.onDidChangeTextDocument(({ document, contentChanges }) => {
+    vscode.workspace.onDidChangeTextDocument(async ({ document, contentChanges }) => {
         const textEditor = vscode.window.activeTextEditor
 
         if (
@@ -19,14 +19,17 @@ export default () => {
                 ['.'],
             ) &&
             /;.$/.test(line.text)
-        )
-            void textEditor.edit(
+        ) {
+            await textEditor.edit(
                 editBuilder => {
                     const endPos = contentChanges[0]!.range.end
                     editBuilder.delete(new vscode.Range(endPos.translate(0, -1), endPos))
                 },
                 { undoStopAfter: false, undoStopBefore: false },
             )
+            await vscode.commands.executeCommand('editor.action.triggerSuggest')
+        }
+
         if (
             equals(
                 contentChanges.map(({ text }) => text),
@@ -34,8 +37,8 @@ export default () => {
             ) &&
             document.lineAt(contentChanges[0]!.range.end).text.endsWith(';)')
         )
-            void textEditor.edit(editBuilder => {
-                const endPos = contentChanges[0]!.range.end
+            await textEditor.edit(editBuilder => {
+                const endPos = document.lineAt(contentChanges[0]!.range.end).range.end.translate(0, -1)
                 editBuilder.delete(new vscode.Range(endPos.translate(0, -1), endPos))
             })
     })
