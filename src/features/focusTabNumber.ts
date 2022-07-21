22aca2624b4d6a748df092b9db80862ecaf35d8e
Vitaly Turovsky <vital2580@icloud.com>
2022-07-21 22:32:18 +0300

    wip impl first cmd

--- src/features/focusTabNumber.ts
+++ src/features/focusTabNumber.ts
@@ -1,6 +1,15 @@
+import * as vscode from 'vscode'
 import { getExtensionSetting, registerExtensionCommand } from 'vscode-framework'
 
 export default () => {
-    if (!getExtensionSetting('features.showTabNumbers')) return
+    const focusTabFromLeft = async (number: number) => {
+        const tabDocument = vscode.window.tabGroups.activeTabGroup.tabs[number]?.input as vscode.TextDocument | undefined
+        if (!tabDocument) return
+        await vscode.window.showTextDocument(tabDocument)
+    };
+
+    registerExtensionCommand('focusTabByNumberFromLeft', async (_, number) => focusTabFromLeft(number))
+    const mode = getExtensionSetting('features.showTabNumbers');
+    if (mode === 'disabled') return
     registerExtensionCommand('focusTabByNumber', () => {})
 }
