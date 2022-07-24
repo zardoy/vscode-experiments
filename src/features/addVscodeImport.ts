b505ea6b21d6b7a5e44e0c5fc15e6a45e7e3ea61
Vitaly Turovsky <vital2580@icloud.com>
2022-07-24 16:47:27 +0300

    fix: always show quick fix to add vscode import

--- src/features/addVscodeImport.ts
+++ src/features/addVscodeImport.ts
@@ -1,22 +1,29 @@
 import * as vscode from 'vscode'
 import { getExtensionSetting } from 'vscode-framework'
 import { notFoundModule } from '../codeActions'
+import {} from '@zardoy/vscode-utils/build/'
 
 export const registerAddVscodeImport = () => {
     if (!getExtensionSetting('features.missingVscodeImportCodeAction')) return
     vscode.languages.registerCodeActionsProvider(['typescript'], {
         provideCodeActions(document, range, context, token) {
             const { diagnostics } = context
-            const problem = diagnostics[0]
-            if (!problem) return
+            let module: string | boolean | undefined
+            let fixingDiagnostic: vscode.Diagnostic | undefined
+            for (const diagnostic of diagnostics) {
+                module = notFoundModule(diagnostic)
+                if (module) {
+                    fixingDiagnostic = diagnostic
+                    break
+                }
+            }
 
-            const module = notFoundModule(problem)
-            if (!module || module !== 'vscode') return
+            if (!fixingDiagnostic || !module || module !== 'vscode') return
 
             const importFix = new vscode.CodeAction('Add vscode import', vscode.CodeActionKind.QuickFix)
             importFix.edit = new vscode.WorkspaceEdit()
             importFix.isPreferred = true
-            importFix.diagnostics = [problem]
+            importFix.diagnostics = [fixingDiagnostic]
             importFix.edit.insert(document.uri, new vscode.Position(0, 0), "import * as vscode from 'vscode'\n")
 
             return [importFix]
