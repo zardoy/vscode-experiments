da23368dd1e927df0b0aaf68b51fcd58eacab474
Vitaly Turovsky <vital2580@icloud.com>
2022-09-04 17:36:52 +0300

    add some imports

--- src/features/executeVscodeScript.ts
+++ src/features/executeVscodeScript.ts
@@ -6,7 +6,12 @@ export default () => {
         if (process.env.PLATFORM === 'web') {
             noWebSupported()
         } else {
+            const { tmpdir } = await import('os')
+            const { join } = await import('path/posix')
             const { execa } = await import('execa')
+            const { existsSync, readdirSync, mkdirSync, writeFileSync } = await import('fs')
+            const dir = join(tmpdir(), 'vscode-exec-script')
+            if (!existsSync(dir)) mkdirSync(dir)(readdirSync(dir).slice(-1)[0])
             // execa('tsc', [''])
         }
     })
