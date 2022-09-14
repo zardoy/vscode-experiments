2920f22280d47f57070eb91e2219eb67b101dc58
Vitaly Turovsky <vital2580@icloud.com>
2022-09-14 12:13:55 +0300

    fix: auto indenting empty lines on commands & shortcuts such as ctrl+z, add setting that by default doing nothing on saved files

--- src/features/indentEmptyLineOnClick.ts
+++ src/features/indentEmptyLineOnClick.ts
@@ -7,6 +7,8 @@ export default () => {
     if (autoIndentEmptyLine === 'disabled') return
     vscode.window.onDidChangeTextEditorSelection(async ({ textEditor, selections, kind }) => {
         if (getActiveRegularEditor()?.document.uri !== textEditor.document.uri) return
+        if (kind === undefined || kind === vscode.TextEditorSelectionChangeKind.Command) return
+        if (!getExtensionSetting('autoIndentEmptyLineIfNotDirty') && !textEditor.document.isDirty) return
         if (autoIndentEmptyLine === 'mouse-only' && kind !== vscode.TextEditorSelectionChangeKind.Mouse) return
         const firstSelection = selections[0]!
         if (selections.length !== 1 || !firstSelection.start.isEqual(firstSelection.end) || firstSelection.start.character !== 0) return
