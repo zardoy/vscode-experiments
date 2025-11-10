b48723fe0d63778f71ebf4fa0888c532ac5a87e2
Vitaly Turovsky <vital2580@icloud.com>
2025-11-10 18:21:25 +0300

    always stage why not

--- src/features/gitMisc.ts
+++ src/features/gitMisc.ts
@@ -8,8 +8,8 @@ export default () => {
         if (!repo) return
         const currentUrl = vscode.window.activeTextEditor?.document.uri
         if (!currentUrl) return
+        await vscode.commands.executeCommand('git.stage')
         if ([...repo.state.indexChanges, ...repo.state.workingTreeChanges, ...repo.state.mergeChanges].some(x => x.uri.fsPath === currentUrl.fsPath)) {
-            await vscode.commands.executeCommand('git.stage')
             return
         }
 
