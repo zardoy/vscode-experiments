5af64d8b837688c375d0c5282d6563aac75d8c69
Vitaly Turovsky <vital2580@icloud.com>
2025-11-10 18:52:00 +0300

    fix: ensure to always run stage

--- src/features/gitMisc.ts
+++ src/features/gitMisc.ts
@@ -4,11 +4,11 @@ import { getGitActiveRepoOrThrow } from '../git-api'
 
 export default () => {
     registerExtensionCommand('fixedGitStageFile', async () => {
+        await vscode.commands.executeCommand('git.stage')
         const repo = getGitActiveRepoOrThrow()
         if (!repo) return
         const currentUrl = vscode.window.activeTextEditor?.document.uri
         if (!currentUrl) return
-        await vscode.commands.executeCommand('git.stage')
         if ([...repo.state.indexChanges, ...repo.state.workingTreeChanges, ...repo.state.mergeChanges].some(x => x.uri.fsPath === currentUrl.fsPath)) {
             return
         }
